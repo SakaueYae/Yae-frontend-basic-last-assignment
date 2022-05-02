174e323145af718369e35af31a380165b712eb49
SakaueYae <y.sakaue@4nonome.com>
2022-05-02 20:05:37 +0900

    Molecules/Messageの作成（仮３）

--- src/stories/molecules-message.stories.js
+++ src/stories/molecules-message.stories.js
@@ -1,5 +1,5 @@
 import Molecules_Message from "../components/molecules/message/message.html";
-import "../components/molecules/message/message.html";
+import "../components/molecules/message/message.scss";
 
 export default { title: "Molecules/Message" }
 
