bab1d9cf246733eb904a6a62ddfefdab6e0acd4a
SakaueYae <y.sakaue@4nonome.com>
2022-05-28 20:38:12 +0900

    htmlファイルの編集

---
+++ src/stories/molecules-card.stories.js
@@ -0,0 +1,6 @@
+import Molecules_Card from "../components/molecules/card/card.html";
+import "../components/molecules/card/card.scss";
+
+export default { title: "Molecules/Card" }
+
+export const Message = () => Molecules_Card;
