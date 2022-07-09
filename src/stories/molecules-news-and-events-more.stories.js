53276d70ed5bb3ae445c0eb0ffd4cb150399674b
SakaueYae <y.sakaue@4nonome.com>
2022-07-09 17:03:32 +0900

    すべてのファイルにprettierを適用

--- src/stories/molecules-news-and-events-more.stories.js
+++ src/stories/molecules-news-and-events-more.stories.js
@@ -1,6 +1,6 @@
 import Molecules_Newsandeventsmore from "../components/molecules/news-and-events-more/news-and-events-more.html";
 import "../components/molecules/news-and-events-more/news-and-events-more.scss";
 
-export default { title: "Molecules/Newsandeventsmore" }
+export default { title: "Molecules/Newsandeventsmore" };
 
 export const Message = () => Molecules_Newsandeventsmore;
