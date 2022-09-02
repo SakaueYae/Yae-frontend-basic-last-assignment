ae753e69e5bc1cbe74cd7859f7f33b5819f1488f
SakaueYae <y.sakaue@4nonome.com>
2022-09-02 19:36:41 +0900

    Templates/NewsAndEventsの作成

---
+++ src/stories/templates-news-and-events.stories.js
@@ -0,0 +1,6 @@
+import Templates_Newsandevents from "../components/templates/news-and-events/news-and-events.html";
+import "../components/templates/news-and-events/news-and-events.scss";
+
+export default { title: "Templates/Newsandevents" };
+
+export const Newsandevents = () => Templates_Newsandevents;
